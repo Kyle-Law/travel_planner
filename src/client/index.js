49fbc480aee066c297bd4b1c3c693123044a54c7
kyle-law <junwei_1121@hotmail.com>
2020-06-11 12:27:03 +0800

    Refinement

--- src/client/index.js
+++ src/client/index.js
@@ -1,22 +1,2 @@
-// import { checkForName } from './js/nameChecker';
-// import { handleSubmit } from './js/formHandler';
-// import './styles/resets.scss';
-// import './styles/base.scss';
-// import './styles/footer.scss';
-// import './styles/form.scss';
-// import './styles/header.scss';
-// import { validateContent } from './js/validator';
-
-
-// console.log(checkForName);
-
-// alert("I EXIST")
-
-// export {
-//     handleSubmit,
-//     validateContent
-// }
-
 import "./js/app.js";
 import "./styles/main.scss";
-// import "./"
